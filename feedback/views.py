a8bbe7046c425911701c9acb8dac0eff1bc2012b
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2016-07-06 10:15:59 +0300

    Add test api subsystem that has static responses from a-plus api

--- feedback/views.py
+++ feedback/views.py
@@ -59,7 +59,7 @@ class FeedbackSubmission(AplusGraderMixin, FormView):
     success_url = '/feedback/'
 
     def get_form_class(self):
-        data = self.grading_data._get_item('form_spec') if self.grading_data else None
+        data = self.grading_data.exercise._get_item('form_spec') if self.grading_data else None
         if not data and settings.DEBUG:
             data = TEST_FORM
 
