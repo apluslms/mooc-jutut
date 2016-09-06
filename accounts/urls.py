6a6d01eef3339e6ecf46f98e11fe267bc22bcfb6
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2016-09-06 03:35:41 +0300

    Use callable instead of string

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+import django_lti_login.views
 
 
 urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
-    url(r'^lti_login$', 'django_lti_login.views.lti_login', name='lti_login'),
+    url(r'^lti_login$', django_lti_login.views.lti_login, name='lti_login'),
 ]
