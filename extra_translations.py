fae939ab65a17e4ef6028c76c54d0e8984a709ab
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2016-11-24 19:38:54 +0200

    Update translations

---
+++ extra_translations.py
@@ -0,0 +1,10 @@
+"""
+This file contains strings that do not have translations in libraries,
+but we still need them to be translated.
+
+This is easier method than finding some hard way to add translations
+for specifig files or modules from libaries to project locales files.
+"""
+from django.utils.translation import ugettext_lazy as _
+
+_('This field is required.')
