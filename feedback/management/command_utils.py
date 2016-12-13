7290350200e5f36525c4182b4697bb01de08439b
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2016-12-13 18:15:35 +0200

    Split get_courses out of get_feedback_queryset

--- feedback/management/command_utils.py
+++ feedback/management/command_utils.py
@@ -7,30 +7,15 @@ from ..models import (
 )
 
 
-def get_feedback_queryset(self,
-                          feedback_id=None,
-                          site_domain=None,
-                          course_code=None):
-    feedbacks = Feedback.objects.select_related('exercise', 'exercise__course', 'exercise__course__namespace').all()
-    feedbacks_c = 0
-    sites = None
-    courses = None
+def get_courses(command,
+                site_domain=None,
+                course_code=None,
+                single_course=False):
 
-    if feedback_id is not None:
-        feedbacks = feedbacks.filter(id=feedback_id)
-        feedbacks_c = feedbacks.count()
-        if feedbacks_c == 0:
-            raise CommandError("Feedback with id {} doesn't exist.".format(feedback_id))
-        return feedbacks, feedbacks_c
-
-    sites = Site.objects.all().all()
+    sites = Site.objects.all()
     if site_domain == 'all':
         if course_code == 'all':
-            feedbacks_c = feedbacks.count()
-            if feedbacks_c == 0:
-                raise CommandError("No feedbacks in the system,")
-            self.stdout.write(self.style.SUCCESS("Fount a total of {} feedbacks".format(feedbacks_c)))
-            return feedbacks, feedbacks_c
+            return None
         elif sites.count() == 0:
             raise CommandError("No sites in the system.")
     elif site_domain:
@@ -38,9 +23,9 @@ def get_feedback_queryset(self,
         if sites.count() == 0:
             raise CommandError("No sites found with domain '{}'".format(site_domain))
     else:
-        self.stdout.write(self.style.NOTICE("List of possible site domains:"))
+        command.stdout.write(command.style.NOTICE("List of possible site domains:"))
         for site in Site.objects.all():
-            self.stdout.write(self.style.SUCCESS("  {}".format(site.domain)))
+            command.stdout.write(command.style.SUCCESS("  {}".format(site.domain)))
         raise CommandError("No site domain or feedback id given")
 
     courses = Course.objects.select_related('namespace').all()
@@ -52,16 +37,40 @@ def get_feedback_queryset(self,
     if courses_c == 0:
         raise CommandError("No courses found")
 
-    self.stdout.write(self.style.NOTICE("Selected courses:"))
+    command.stdout.write(command.style.NOTICE("Selected courses:"))
     courses_out = ["{}: {} - {}".format(course.namespace.domain, course.code, course.name) for course in courses]
     for course in sorted(courses_out):
-        self.stdout.write(self.style.SUCCESS("  {}".format(course)))
+        command.stdout.write(command.style.SUCCESS("  {}".format(course)))
 
-    if not course_code and courses_c != 1:
+    if single_course and not course_code and courses_c != 1:
         raise CommandError("More than one course found. Use flag -c to lomit")
 
-    feedbacks = feedbacks.filter(exercise__course__in=courses)
+    return courses
+
+
+def get_feedback_queryset(command,
+                          feedback_id=None,
+                          site_domain=None,
+                          course_code=None):
+    feedbacks = Feedback.objects.select_related('exercise', 'exercise__course', 'exercise__course__namespace').all()
+    feedbacks_c = 0
+
+    # single
+    if feedback_id is not None:
+        feedbacks = feedbacks.filter(id=feedback_id)
+        feedbacks_c = feedbacks.count()
+        if feedbacks_c == 0:
+            raise CommandError("Feedback with id {} doesn't exist.".format(feedback_id))
+        return feedbacks, feedbacks_c
+
+    # multiple
+    courses = get_courses(command, site_domain, course_code, True)
+    if courses:
+        feedbacks = feedbacks.filter(exercise__course__in=courses)
     feedbacks_c = feedbacks.count()
-    self.stdout.write(self.style.SUCCESS("Fount a total of {} feedbacks".format(feedbacks_c)))
+
+    if not courses and not feedbacks_c:
+        raise CommandError("No feedbacks in the system,")
+    command.stdout.write(command.style.SUCCESS("Fount a total of {} feedbacks".format(feedbacks_c)))
 
     return feedbacks, feedbacks_c
