cc8e9909d4e7d8f0bf25f9de7c34a0f054eab322
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2017-10-16 19:41:54 +0300

    Add frontpage for those that stumble on it

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.views.decorators.cache import cache_page
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -11,6 +12,9 @@ def cache(time=60 * 15):
 
 
 urlpatterns = [
+    url(r'^$',
+        TemplateView.as_view(template_name="core/frontpage.html"),
+        name='fronpage'),
     url(r'^servicestatus/$',
         views.ServiceStatusPage.as_view(),
         name='servicestatus'),
