e3f1a8059f2f8e630b162af07e3ee16f10b9a194
Joni Toivonen <joni.toivonen@aalto.fi>
2022-11-14 09:22:17 +0200

    Fix hardcoded path in lint script

--- lint.sh
+++ lint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -f .github/workflows/Dockerfile -q . -t prospector
-docker run -v /u/88/toivonj15/unix/projects/mooc-jutut:/app -w /app prospector sh -c 'prospector'
+docker run -v ${PWD}:/app -w /app prospector sh -c 'prospector'
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=prospector --format="{{.ID}}"))
